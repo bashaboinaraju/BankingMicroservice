0d728f34794f283652beaaa40f00d489a2f353e1
bashaboinaraju <bashaboinaraju934@gmail.com>
2024-11-05 01:22:01 +0530

    Update App.java

--- src/main/java/com/raju/App.java
+++ src/main/java/com/raju/App.java
@@ -5,6 +5,6 @@ package com.raju;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello amma");
+        System.out.println("Hello india");
     }
 }
